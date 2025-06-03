5be4ac8852bafc2fc9e14ea7db3bd5b82acb2023
Lothean0 <xxsimaoxx1@gmail.com>
2025-06-03 15:41:41 +0100

    .

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC2164
 cd "$(dirname "$0")"
+
 # Define paths
 SRC_DIR="Server/src"
 BIN_DIR="out/production/Server"
@@ -13,6 +14,7 @@ echo "Compiling Erlang source files..."
 erlc -o "$BIN_DIR" $SRC_DIR/*.erl
 
 # Check if compilation was successful
+# shellcheck disable=SC2181
 if [ $? -ne 0 ]; then
   echo "Compilation failed. Exiting."
   exit 1
@@ -21,4 +23,5 @@ fi
 echo "Compilation successful. Starting the server..."
 
 # Start the Erlang VM and run the server
-erl -pa "$BIN_DIR" -eval "server:start(8000)."
+cd "$BIN_DIR"
+erl -eval "server:start(8000), timer:sleep(infinity)."
